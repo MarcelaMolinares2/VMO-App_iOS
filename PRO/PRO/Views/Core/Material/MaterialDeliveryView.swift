ad3b4d416f61a6249ffd4beffb40c37b1678e5d5
aliumInc <88171659+aliumInc@users.noreply.github.com>
2021-10-07 07:08:26 -0500

    Implemento de fab y view a otra view
    Cree un fab button y estoy viendo para pasar de view cuando se da click en este

--- PRO/PRO/Views/Core/Material/MaterialDeliveryView.swift
+++ PRO/PRO/Views/Core/Material/MaterialDeliveryView.swift
@@ -18,6 +18,38 @@ struct MaterialDeliveryView: View {
     
     var body: some View {
         ZStack {
+            NavigationLink(destination: MaterialRequestView()) {
+                Text("Next View")
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("HOLA")
+                    }, label: {
+                        Text("+")
+                        .font(.system(.largeTitle))
+                        .frame(width: 77, height: 70)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 7)
+                    })
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                }
+            }
+                
+            
+            
+        }
+    }
+        /*
+        ZStack {
             VStack {
                 HeaderToggleView(couldSearch: false, title: "modMaterialDelivery", icon: Image("ic-material"), color: Color.cPanelMaterial)
                 ZStack(alignment: .bottomTrailing) {
@@ -59,5 +91,7 @@ struct MaterialDeliveryView: View {
     func load() {
         
     }
+    */
     
 }
+
--- PRO/PRO/Views/Core/Material/MaterialRequestView.swift
+++ PRO/PRO/Views/Core/Material/MaterialRequestView.swift
@@ -7,10 +7,57 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct MaterialRequestView: View {
+    
+    
+    @ObservedObject private var selectMaterialsModalToggle = ModalToggle()
+    
+    @State private var selectedMaterials = [String]()
+    @State private var deliveries = [AdvertisingMaterialDelivery]()
+    
     var body: some View {
-        Text("Hello, dkdk")
+        ZStack {
+            VStack {
+                HeaderToggleView(couldSearch: false, title: "modMaterialDelivery", icon: Image("ic-material"), color: Color.cPanelMaterial)
+                ZStack(alignment: .bottomTrailing) {
+                    Button(action: {
+                        selectMaterialsModalToggle.status.toggle()
+                    }) {
+                        Text("...Material prueba...")
+                    }
+                }
+                List {
+                    ForEach(deliveries, id: \.materialId) { item in
+                        Text("MAT \(item.materialId)")
+                        Text(item.material?.name ?? "--")
+                    }
+                }
+            }
+            if selectMaterialsModalToggle.status {
+                GeometryReader {geo in
+                    CustomDialogPicker(modalToggle: selectMaterialsModalToggle, selected: $selectedMaterials, key: "MATERIAL", multiple: true)
+                }
+                .background(Color.black.opacity(0.45))
+                .onDisappear {
+                    selectedMaterials.forEach { id in
+                        if let material = MaterialDao(realm: try! Realm()).by(id: id) {
+                            let delivery = AdvertisingMaterialDelivery()
+                            delivery.materialId = material.id
+                            delivery.material =  material
+                            deliveries.append(delivery)
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {
+            load()
+        }
+    }
+    
+    func load() {
         
     }
 }
