2af733e6945f42c5e12a9df9d4c270f9368041ce
aliumInc <88171659+aliumInc@users.noreply.github.com>
2021-11-24 14:58:54 -0500

    Changes Pod Update
    Tambien cambios en el archivo de RequestDatView

--- PRO/PRO/Views/RequestDayView.swift
+++ PRO/PRO/Views/RequestDayView.swift
@@ -22,8 +22,10 @@ struct RequestDayView: View {
     @State private var isValidationOn = false
     
     @State private var selectedCycle = [String]()
+    @State private var selectedRequestReason = [String]()
     
     @ObservedObject var selectCycleModalToggle = ModalToggle()
+    @ObservedObject var selectReasonModalToggle = ModalToggle()
     
     init() {
         UITableView.appearance().tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
@@ -59,12 +61,10 @@ struct RequestDayView: View {
                         }
                         DatePicker("envDateStart", selection: $dateStart, in: Date()..., displayedComponents: .date)
                             .foregroundColor(.cTextHigh)
-                            
                             .onChange(of: dateStart, perform: { value in
                                 noDays = String(Calendar.current.dateComponents([.day], from: dateStart, to: dateEnd).day! + 1)
                             });
                         DatePicker("envDateEnd", selection: $dateEnd, in: Date()..., displayedComponents: .date)
-                            
                             .foregroundColor(.cTextHigh)
                             .onChange(of: dateEnd, perform: { value in
                                 noDays = String(Calendar.current.dateComponents([.day], from: dateStart, to: dateEnd).day! + 1)
@@ -86,12 +86,23 @@ struct RequestDayView: View {
                                 .foregroundColor(isValidationOn && percentageValue <= 0 ? .cDanger : .cTextHigh)
                             Slider(value: $percentageValue, in: 0.0...4, step: 1)
                         }
+                        Button(action: {
+                            selectReasonModalToggle.status.toggle()
+                        }) {
+                            VStack {
+                                Text("envDayRequestReason")
+                            }
+                            .font(!selectedCycle.isEmpty ? .system(size: 14.0) : .none)
+                            .foregroundColor(!selectedCycle.isEmpty ? .cTextMedium : (isValidationOn ? .cDanger : .cTextHigh))
+                        }
+                        /*
                         Picker(selection: $selectedReason, label: Text("envDayRequestReason")) {
                             ForEach(0 ..< reasons.count) {
                                 Text(self.reasons[$0].content ?? "").tag(self.reasons[$0].id)
                             }
                         }
                         .foregroundColor(isValidationOn && selectedReason == -1 ? .cDanger : .cTextHigh)
+                        */
                         VStack {
                             TextField(NSLocalizedString("envComment", comment: ""), text: $comment)
                                 .frame(height: 30)
@@ -119,9 +130,16 @@ struct RequestDayView: View {
             }
             if selectCycleModalToggle.status {
                 GeometryReader {geo in
-                    CustomDialogPicker(onSelectionDone: onSelectionDone, selected: $selectedCycle, key: "CYCLE")
+                    CustomDialogPicker(onSelectionDone: onSelectionCycleDone, selected: $selectedCycle, key: "CYCLE")
                 }
                 .background(Color.black.opacity(0.45))
+            }
+            if selectReasonModalToggle.status {
+                GeometryReader {geo in
+                    CustomDialogPicker(onSelectionDone: onSelectionReasonDone, selected: $selectedRequestReason, key: "STYLE")
+                }
+                .background(Color.black.opacity(0.45))
+                
             }
         }
         .onAppear {
@@ -152,8 +170,12 @@ struct RequestDayView: View {
         dayRequest.dateEnd = dateEnd
     }
     
-    func onSelectionDone(_ selected: [String]) {
+    func onSelectionCycleDone(_ selected: [String]) {
         selectCycleModalToggle.status.toggle()
+    }
+    
+    func onSelectionReasonDone(_ selected: [String]) {
+        selectReasonModalToggle.status.toggle()
     }
 }
 
